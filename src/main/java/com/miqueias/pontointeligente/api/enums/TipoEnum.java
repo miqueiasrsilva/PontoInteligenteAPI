934ae70db5631c620666a09d998064e972445d39
miqueiasrsilva <mi.ike@hotmail.com>
2020-11-02 17:25:58 -0200

    Adicionando entidades principais

---
+++ src/main/java/com/miqueias/pontointeligente/api/enums/TipoEnum.java
@@ -0,0 +1,10 @@
+package com.miqueias.pontointeligente.api.enums;
+
+public enum TipoEnum {
+	INICIO_TRABALHO,
+	TERMINO_TRABALHO,
+	INICIO_ALMOCo,
+	TERMINO_ALMOCO,
+	INICIO_PAUSA,
+	TERMINO_PAUSA
+}
